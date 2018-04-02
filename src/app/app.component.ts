8d32c419ee74b8070740b47fe9b01dcf6df97453
Narve Saetre <narve@dv8.no>
2018-04-02 13:57:43 +0200

    - Added npm packages for bpmn-js and properties panel - Added imports and code to AppComponent - Added style imports in angular-cli.json and some styling in app.component.css. - Fixed old-style angular code, added bpmn-diagram to avoid CORS problems

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,61 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+//
+import * as BpmnJS from 'bpmn-js/lib/Modeler.js';
+import * as panelModule from 'bpmn-js-properties-panel';
+import * as providerModule from 'bpmn-js-properties-panel/lib/provider/bpmn';
+// import * as providerModule from 'bpmn-js-properties-panel/lib/provider/camunda';
+// import * as data from 'camunda-bpmn-moddle/resources/camunda.json';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  viewer: BpmnJS;
+
+  constructor(private http: HttpClient) {
+  }
+
+  ngOnInit(): void {
+    this.viewer = new BpmnJS({
+      container: '#canvas',
+      width: '100%',
+      height: '600px',
+      additionalModules: [
+        panelModule,
+        providerModule
+      ],
+      propertiesPanel: {
+        parent: '#properties'
+      },
+      moddleExtensions: {
+        // camunda: data
+      }
+    });
+  }
+
+  handleError(err: any) {
+    if (err) {
+      console.warn('Ups, error: ', err);
+    }
+  }
+
+  load(): void {
+    // const url = 'https://raw.githubusercontent.com/bpmn-io/bpmn-js/master/resources/initial.bpmn';
+    const url = '/assets/bpmn/initial.bpmn';
+    this.http.get(url, {
+      headers: {observe: 'response'}, responseType: 'text'
+    })
+      .subscribe(
+        (x: any) => {
+          console.log('got something: ', x);
+          this.viewer.importXML(x, this.handleError);
+        },
+        this.handleError
+      );
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import {HttpClientModule, HttpClient, HttpHandler} from '@angular/common/http';
+
 
 
 @NgModule({
@@ -10,7 +12,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
