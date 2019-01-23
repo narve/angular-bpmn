ea066dc8319067967321fb8ba8fb310c4c9cb798
Narve Sætre <narve.saetre@ae.no>
2019-01-23 12:42:36 +0100

    inject ElementFactory in CustomPaletteProvider for fun and profit

--- src/app/props-provider/CustomPaletteProvider.ts
+++ src/app/props-provider/CustomPaletteProvider.ts
@@ -2,16 +2,19 @@ import {IPalette, IPaletteProvider} from "../bpmn-js/bpmn-js";
 
 export class CustomPaletteProvider implements IPaletteProvider {
 
-  static $inject = ['palette', 'originalPaletteProvider'];
+  static $inject = ['palette', 'originalPaletteProvider', 'elementFactory'];
+
+  private readonly elementFactory: any;
 
   // Note that names of arguments must match injected modules, see InjectionNames.
   // I don't know why originalPaletteProvider matters but it breaks if it isn't there.
   // I guess since this component is injected, and it requires an instance of originalPaletteProvider,
   // originalPaletteProvider will be new'ed and thus call palette.registerProvider for itself.
   // There probably is a better way.
-  constructor(private palette: IPalette, private originalPaletteProvider: IPaletteProvider) {
+  constructor(private palette: IPalette, private originalPaletteProvider: IPaletteProvider, elementFactory) {
     // console.log(this.constructor.name, "constructing", palette, originalPaletteProvider);
     palette.registerProvider(this);
+    this.elementFactory = elementFactory;
   }
 
   getPaletteEntries() {
@@ -22,9 +25,9 @@ export class CustomPaletteProvider implements IPaletteProvider {
         className: ['fa-save', 'fa'],
         title: 'TEST',
         action: {
-          click: () => console.log( 'TEST Action clicked!')
+          click: () => console.log( 'TEST Action clicked! Elementfactory: ', this.elementFactory)
         }
-      },
+      }
     };
   }
 }
