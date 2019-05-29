a0112295c9368c37f2749d8bd664ad41379dd123
timelot <timmmichaelfreitag@hotmail.com>
2019-05-29 14:51:18 +0200

    added context-pad support

--- src/app/bpmn-js/bpmn-js.ts
+++ src/app/bpmn-js/bpmn-js.ts
@@ -4,6 +4,7 @@ import * as _PropertiesPanelModule from 'bpmn-js-properties-panel';
 import * as _BpmnPropertiesProvider from 'bpmn-js-properties-panel/lib/provider/bpmn';
 import * as _EntryFactory from 'bpmn-js-properties-panel/lib/factory/EntryFactory';
 import _PaletteProvider from 'bpmn-js/lib/features/palette/PaletteProvider';
+import _ContextPadProvider from 'bpmn-js/lib/features/context-pad/ContextPadProvider';
 
 export const InjectionNames = {
   eventBus: 'eventBus',
@@ -14,6 +15,8 @@ export const InjectionNames = {
   bpmnPropertiesProvider: 'bpmnPropertiesProvider',
   paletteProvider: 'paletteProvider',
   originalPaletteProvider: 'originalPaletteProvider',
+  contextPadProvider: 'contextPadProvider',
+  originalContextPadProvider: 'originalContextPadProvider',
 };
 
 export const Modeler = _Modeler;
@@ -21,6 +24,7 @@ export const PropertiesPanelModule = _PropertiesPanelModule;
 export const EntryFactory = _EntryFactory;
 export const OriginalPaletteProvider = _PaletteProvider;
 export const OriginalPropertiesProvider = _BpmnPropertiesProvider;
+export const OriginalContextPadProvider = _ContextPadProvider;
 
 export interface IPaletteProvider {
   getPaletteEntries(): any;
@@ -32,4 +36,12 @@ export interface IPalette {
 
 export interface IPropertiesProvider {
   getTabs(elemnt): any;
+}
+
+export interface IContextPad {
+  registerProvider(provider: IContextPadProvider): any;
+}
+
+export interface IContextPadProvider {
+  getContextPadEntries(elemnt): any;
 }
